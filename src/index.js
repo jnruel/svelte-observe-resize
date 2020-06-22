046fff285c015374beb15f9cd55f8f251aa1dbe2
axtn95 <axtn95@gmail.com>
2020-06-21 23:27:55 -0700

    Singleton resize observer and pass the resizeObserverEntry in event detail

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import ResizeObserver from 'resize-observer-polyfill';
 
+// Singleton observer for all nodes
+const resizeObserer = new ResizeObserver((entries) => {
+  // Dispatch custom event when each node is resized
+  entries.forEach(entry => {
+    entry.target.dispatchEvent(new CustomEvent('resize', { detail: entry }));
+  })
+});
+
 /**
  * Custom Svelte action
  *  - Uses Resize Observer API
@@ -8,15 +16,12 @@ import ResizeObserver from 'resize-observer-polyfill';
  * Example usage: `<div use:watchResize on:resize={handleResize}></div>`
  *
  * @param HTMLElement node
+ * @param String box - sets the observe option for box. Can be set to 'content-box' (default) or 'border-box'
  */
-export default function observeResize(node) {
-  const resizeObserer = new ResizeObserver((entries) => {
-    // Dispatch custom event when node is resized
-    node.dispatchEvent(new CustomEvent('resize'));
-  });
+export default function observeResize(node, box) {
 
   // Watch for node to resize
-  resizeObserer.observe(node);
+  resizeObserer.observe(node, box && { box } );
 
   return {
     // Called when node is unmounted
